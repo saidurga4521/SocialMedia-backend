9830acffde7893fdfbb55f33ba3a2159c7a5b656
Durga Prasad <saikamireddi3@gmail.com>
2025-07-22 13:57:36 +0530

    creating user model

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt");
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name should be required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email should be required"],
+      unique: [true, "email should be unique"],
+    },
+    password: {
+      type: String,
+      required: [true, "password should be required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//hashing the password
+userSchema.pre("save", async (next) => {
+  const salt = await bcrypt.genSalt(15);
+  const hashedPasword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPasword;
+  next();
+});
+const User = model("User", userSchema);
+module.exports = User;
--- readme.md
+++ readme.md
@@ -11,3 +11,15 @@
      - mongoose.connect(process.env.MONGO_URI) connects your Node.js app to MongoDB using the URI from .env.
 
      - mongoose.connection is used to monitor the connection; db.on("error") logs if there's an error, and db.once("open") confirms a successful connection.
+
+#### 2. Authentication
+
+##### a.creating user model:
+
+        - Defines a Mongoose schema for User with name, email, and password as required fields; email must be unique.
+
+        - Uses a pre-save hook to hash the password before saving to the database.
+
+        - In the hook, bcrypt generates a salt (genSalt) and hashes the password, replacing the plain text password with the hashed one.
+
+        - Exports the User model created from the schema for use in other parts of the application.
