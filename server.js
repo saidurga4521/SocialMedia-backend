451b92073522db61780e20a3ad77709b61b52355
Durga Prasad <saikamireddi3@gmail.com>
2025-07-31 13:07:27 +0530

    adding cors error

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 require("./config/mongoose");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use("/api", require("./routes"));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 app.get("/", (req, res) => {
   res.status(200).send({
     message: "Server is running",
