ee8aa775bfe4181c43f5e7b1fc205b796a4420e0
Durga Prasad <saikamireddi3@gmail.com>
2025-07-31 14:52:38 +0530

    cors error

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+      const allowed = ["http://localhost:5173"];
+      if (!origin || allowed.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
-app.use(express.json());
 app.use(express.json());
 app.use("/api", require("./routes"));
 
