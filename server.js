897bb1c7ffe1b656beb76efbecd74217c13ca3ca
Durga Prasad <saikamireddi3@gmail.com>
2025-09-07 13:31:56 +0530

    add star

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: function (origin, callback) {
-      const allowed = ["http://localhost:5173"];
-      if (!origin || allowed.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    // origin: function (origin, callback) {
+    //   const allowed = ["http://localhost:5173"];
+    //   if (!origin || allowed.includes(origin)) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error("Not allowed by CORS"));
+    //   }
+    // },
+    // credentials: true,
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "*",
   })
 );
 app.use(
