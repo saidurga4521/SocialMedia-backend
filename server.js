4d0a6d03338937b0b92cdbdd763e7a5d28a94e7b
Durga Prasad <saikamireddi3@gmail.com>
2025-09-13 17:37:48 +0530

    cors error is added

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ const session = require("express-session");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-// app.use(
-//   cors({
-//     // origin: function (origin, callback) {
-//     //   const allowed = ["http://localhost:5173"];
-//     //   if (!origin || allowed.includes(origin)) {
-//     //     callback(null, true);
-//     //   } else {
-//     //     callback(new Error("Not allowed by CORS"));
-//     //   }
-//     // },
-//     // credentials: true,
-//     // methods: ["GET", "POST", "PUT", "DELETE"],
-//     origin: "*",
-//   })
-// );
+app.use(
+  cors({
+    // origin: function (origin, callback) {
+    //   const allowed = ["http://localhost:5173"];
+    //   if (!origin || allowed.includes(origin)) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error("Not allowed by CORS"));
+    //   }
+    // },
+    // credentials: true,
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "*",
+  })
+);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
